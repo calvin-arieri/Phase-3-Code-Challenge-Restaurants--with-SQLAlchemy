c226bfadcf1ac4ffcbd6429e5b01de5649960119
calvin-arieri <calvin.arieri@student.moringaschool.com>
2023-06-04 12:49:12 +0300

    added the initializers

--- lib/Customer.py
+++ lib/Customer.py
@@ -1,2 +1,13 @@
 class Customer:
-    pass
+    def __init__(self, given_name, family_name):
+          self.given_name = given_name
+          self.family_name = family_name
+      
+    
+
+
+    
+
+         
+     
+           
