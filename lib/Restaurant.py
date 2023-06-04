095d7990e9f1d5dcc14ed765febeddc23cb806ad
calvin-arieri <calvin.arieri@student.moringaschool.com>
2023-06-04 14:29:07 +0300

    done with the restuarant instances

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -1,2 +1,12 @@
+# - `Restaurant __init__()`
+#   - Restaurants should be initialized with a name, as a string
+# - `Restaurant name()`
+#   - returns the restaurant's name
+#   - should not be able to change after the restaurant is created
 class Restaurant:
-    pass
+    def __init__(self, restaurant_name):
+        self.restaurant_name = restaurant_name
+
+    def name(self):
+        return self.restaurant_name
+    
