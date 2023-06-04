977b5d2327a52310c9c5a85d3b65456f090c3abc
calvin-arieri <calvin.arieri@student.moringaschool.com>
2023-06-04 16:17:58 +0300

    done with core deliverables

--- lib/Review.py
+++ lib/Review.py
@@ -1,2 +1,19 @@
+from sqlalchemy import String,
 class Review:
-    pass
+    all_reviews = []
+    def __init__(self, restaurant, customer, customer_rating):
+        self.restaurant = restaurant
+        self.customer = customer
+        self.customer_rating = customer_rating
+        Review.all_reviews.append(self)
+
+    def rating(self):
+        return self.customer_rating
+    
+    @classmethod
+    def all(cls):
+        for review in cls.all_reviews:
+            return review
+
+    def customer():
+
