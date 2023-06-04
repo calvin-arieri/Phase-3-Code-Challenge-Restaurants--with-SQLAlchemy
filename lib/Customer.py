b9012e04f257b8210f8be97652d7133be969a577
calvin-arieri <calvin.arieri@student.moringaschool.com>
2023-06-04 14:24:06 +0300

    done with all customers

--- lib/Customer.py
+++ lib/Customer.py
@@ -1,8 +1,44 @@
+# - `Customer __init__()`
+#   - Customer should be initialized with a given name and family name, both strings (i.e., first and last name, like George Washington)"
+# - `Customer given_name()`
+#   - returns the customer's given name
+#   - should be able to change after the customer is created
+# - `Customer family_name()`
+#   - returns the customer's family name
+#   - should be able to change after the customer is created
+# - `Customer full_name()`
+#   - returns the full name of the customer, with the given name and the family name concatenated, Western style.
+# - `Customer all()`
+#   - returns **all** of the customer instances
+
 class Customer:
-    def __init__(self, given_name, family_name):
-          self.given_name = given_name
-          self.family_name = family_name
-      
+    all_customers = []
+
+    def __init__(self, first_name, surname):
+          self.first_name = first_name
+          self.surname = surname
+          Customer.all_customers.append(self)
+
+    def given_name(self):
+         return self.first_name
+    
+    def family_name(self):
+         return self.surname
+    
+    def full_name(self):
+         fullname = self.first_name + " " + self.surname
+         return fullname
+    
+    @classmethod
+    def all(cls):
+       return cls.all_customers 
+         
+
+
+
+
+    
+           
     
 
 
